2105a0a78c6d497fd5d4a82465379943296b62b9
andres <velasco.andrs@gmail.com>
2021-02-01 12:27:09 -0500

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,50 @@
+import 'package:credit_card/credit_card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_payments_example/mocks/tarjetas_credito_mock.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagar'),
-      ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
           ),
-        ),
-      );
+        ],
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            width: size.width,
+            height: size.height,
+            top: 200,
+            child: PageView.builder(
+              controller: PageController(
+                viewportFraction: 0.9,
+              ),
+              physics: BouncingScrollPhysics(),
+              itemCount: tarjetas.length,
+              itemBuilder: (_, i) {
+                final tarjeta = tarjetas[i];
+                return CreditCardWidget(
+                  cardNumber: tarjeta.cardNumber,
+                  expiryDate: tarjeta.expiracyDate,
+                  cardHolderName: tarjeta.cardHolderName,
+                  cvvCode: tarjeta.cvv,
+                  showBackView: false,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
