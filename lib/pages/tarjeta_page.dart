96214929aaecf6eca8e293d72b2c3b28a3fc2566
andres <velasco.andrs@gmail.com>
2021-02-03 12:35:37 -0500

    tarjeta seleccionada con le hero animation desde bloc

--- lib/pages/tarjeta_page.dart
+++ lib/pages/tarjeta_page.dart
@@ -1,22 +1,24 @@
 import 'package:credit_card/credit_card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_payments_example/bloc/pagar/pagar_bloc.dart';
-import 'package:flutter_payments_example/mocks/tarjetas_credito_mock.dart';
 import 'package:flutter_payments_example/widgets/total_pay_button.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TarjetaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final tarjeta = context.read<PagarBloc>().state.tarjeta;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagar'),
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
           onPressed: () {
+            // ignore: close_sinks
             final pagarBloc = context.read<PagarBloc>();
             pagarBloc.add(OnDesactivarTarjeta());
-            Navigator.of(context).pop(); // 
+            Navigator.of(context).pop(); //
           },
         ),
       ),
@@ -24,12 +26,12 @@ class TarjetaPage extends StatelessWidget {
         children: [
           Container(),
           Hero(
-            tag: tarjetas[0].cardNumber,
+            tag: tarjeta.cardNumber,
             child: CreditCardWidget(
-              cardNumber: tarjetas[0].cardNumber,
-              expiryDate: tarjetas[0].expiracyDate,
-              cardHolderName: tarjetas[0].cardHolderName,
-              cvvCode: tarjetas[0].cvv,
+              cardNumber: tarjeta.cardNumber,
+              expiryDate: tarjeta.expiracyDate,
+              cardHolderName: tarjeta.cardHolderName,
+              cvvCode: tarjeta.cvv,
               showBackView: false,
             ),
           ),
